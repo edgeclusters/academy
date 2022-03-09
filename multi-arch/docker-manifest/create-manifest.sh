2d754cc63bb7ef873f7c7b86a275a2659ba70678
amoravek <amoravek@gmail.com>
2022-03-10 00:53:21 +0100

    updates

--- multi-arch/docker-manifest/create-manifest.sh
+++ multi-arch/docker-manifest/create-manifest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker manifest create \
-reg.edgeclusters.com/academy/multiarch-image:1.0-manifest
+reg.edgeclusters.com/academy/multiarch-image:1.0-manifest \
 --amend reg.edgeclusters.com/academy/multiarch-image:amd64 \
 --amend reg.edgeclusters.com/academy/multiarch-image:arm64
+
+docker manifest push reg.edgeclusters.com/academy/multiarch-image:1.0-manifest
