f8e09f785dc1c2efbbf16db7a00fff7f7f49c8ea
amoravek <amoravek@gmail.com>
2022-03-10 00:38:10 +0100

    updates

--- multi-arch/docker-buildx/build.sh
+++ multi-arch/docker-buildx/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-docker run --privileged --rm tonistiigi/binfmt --install all
-#docker run --privileged --rm tonistiigi/binfmt --install arm64
+# docker run --privileged --rm tonistiigi/binfmt --install all
+# docker run --privileged --rm tonistiigi/binfmt --install arm64
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t reg.edgeclusters.com/academy/multiarch-image:1.0-buildx .
+docker buildx create --name mybuilder --driver-opt network=host --use
+
+docker buildx build \
+--platform=linux/amd64,linux/arm64 \
+--tag reg.edgeclusters.com/academy/multiarch-image:1.0-buildx \
+--output=type=registry \
+.
