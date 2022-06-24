ac30c66cf46ea0c7dc40a16fcfc12f12ff578733
amoravek <amoravek@gmail.com>
2022-06-24 10:07:54 +0200

    updates

--- helm/nastenka/image/build-spravne.sh
+++ helm/nastenka/image/build-spravne.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE=reg.edgeclusters.com/academy/nastenka:1.0
+IMAGE=reg.edgeclusters.com/academy/nastenka:1.1
 
 docker buildx build \
 --platform linux/amd64,linux/arm64 \
